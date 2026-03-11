a6d8e0462f27279e28fde7c89d0ff0994d7560c7
Brad <Bradkharris@gmail.com>
2026-03-10 19:14:08 -0700

    loadScene implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { loadDesert } from "./scenes/desert/desert";
+import { loadScene } from "./engine/loadScene";
+import desertScene from "./scenes/desert/scene.json";
 import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `<div id="scene"></div>`;
 
-loadDesert();
+loadScene(desertScene);
