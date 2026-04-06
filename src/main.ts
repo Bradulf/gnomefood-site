cbeaeb3779fa4e5139a4c830981472e730936577
Bradulf <Bradkharris@gmail.com>
2026-04-06 15:39:10 -0700

    making the desert.ts something of use

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { loadScene } from "./engine/loadScene";
-import desertScene from "./scenes/desert/scene.json";
+import {desertScene} from "./scenes/desert/desert.ts";
 import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
